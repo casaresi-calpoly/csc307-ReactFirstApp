d6727d80681539e3a74a40e043fd5e1a4b38ef76
Daniel <dan.casares@gmail.com>
2023-11-04 21:22:43 -0700

    Added delete user by id to backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -58,6 +58,10 @@ const addUser = (user) => {
     return user;
 }
 
+const findUserIndex = (id) => {
+   users['users_list'].findIndex( (user) => user['id'] === id);
+}
+
 app.get('/users', (req, res) => {
     const name = req.query.name;
     if (name != undefined){
@@ -85,3 +89,14 @@ app.post('/users', (req, res) => {
     addUser(userToAdd);
     res.send();
 });
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id'];
+    let index = findUserIndex(id);
+    if (index < 0) {
+        res.status(404).send('Resource not found.');
+    } else {
+        users['users_list'].splice(index, 1);
+        res.send();
+    }
+ });
